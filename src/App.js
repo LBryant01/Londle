f78e3bff81807dc33851a4e4f8903ef3b3011613
LBryant01 <london.bryant.lb@gmail.com>
2025-07-28 08:15:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./Wordles.css";
 import thumbnail from "./thumbnail_7_SWS_patch.png";
 
 const App = () => {
-  const targetWord = "shell".toUpperCase();
+  const targetWord = "voice".toUpperCase();
   const maxAttempts = 6;
   const wordLength = targetWord.length;
 
